c8f74dd6f7402131710b0fe2cbd289a87fbd2fe6
Enelyah <ms.jen.christine@gmail.com>
2020-03-14 19:36:33 +0100

    added category images

--- client/src/components/catalogue/CollectionListing.js
+++ client/src/components/catalogue/CollectionListing.js
@@ -8,6 +8,40 @@ class CollectionListing extends Component {
         categories: []
      }
 
+    images=[
+        "../../../images/categories/abstract.jpg",
+        "../../../images/categories/adventure.jpg",
+        "../../../images/categories/animals.jpg",
+        "../../../images/categories/bluffing.jpg",
+        "../../../images/categories/cardgame.jpg",
+        "../../../images/categories/citybuilding1.jpg",
+        "../../../images/categories/cooperativegames1.jpg",
+        "../../../images/categories/deduction2.jpg",
+        "../../../images/categories/dice.jpg",
+        "../../../images/categories/educational1.jpg",
+        "../../../images/categories/family2.jpg",
+        "../../../images/categories/farming.png",
+        "../../../images/categories/horror.jpg",
+        "../../../images/categories/humor.jpg",
+        "../../../images/categories/medieval.jpg",
+        "../../../images/categories/memory2.jpg",
+        "../../../images/categories/mythology2.jpg",
+        "../../../images/categories/party2.jpg",
+        "../../../images/categories/puzzle.jpg",
+        "../../../images/categories/rpg.jpg",
+        "../../../images/categories/scifi2.jpg",
+        "../../../images/categories/strategy.jpg",
+        "../../../images/categories/wargame.jpg",
+        "../../../images/categories/western.jpg",
+        "../../../images/categories/zombies.jpg",
+    ]
+
+    img = () => {
+        var state2=[...this.state.categories];
+        state2.forEach((cat,i) => cat.image= this.images[i])
+        this.setState({categories:state2})
+    }
+
     componentDidMount = () => {
         axios.get('https://www.boardgameatlas.com/api/game/categories?client_id=FWG6FKSO4N')
           .then(response=> response.data.categories.filter(function(cat) {
@@ -16,8 +50,8 @@ class CollectionListing extends Component {
                 cat.name === "Adventure" ||
                 cat.name === "Animals" ||
                 cat.name === "Bluffing" ||
-                cat.name === "Card game" ||
-                cat.name === "City building" ||
+                cat.name === "Card Game" ||
+                cat.name === "City Building" ||
                 cat.name === "Cooperative" ||
                 cat.name === "Deduction" ||
                 cat.name === "Dice" ||
@@ -29,18 +63,20 @@ class CollectionListing extends Component {
                 cat.name === "Medieval" ||
                 cat.name === "Memory" ||
                 cat.name === "Mythology" ||
-                cat.name === "Party game" ||
+                cat.name === "Party Game" ||
                 cat.name === "Puzzle" ||
                 cat.name === "RPG" ||
                 cat.name === "Sci-Fi" ||
                 cat.name === "Strategy" ||
-                cat.name === "Solo/Solitaire" ||
                 cat.name === "Wargame" ||
                 cat.name === "Western" ||
                 cat.name === "Zombies"
             );
         }))
         .then(filter => this.setState({categories: filter}))
+        .then(
+            this.img
+        )
       }
 
       ;
@@ -48,10 +84,10 @@ class CollectionListing extends Component {
     render() { 
         return ( 
             <div className = "clp">
-                <h1>All categories</h1>
+                <h1>All recommended categories</h1>
                 <div className="listing">
                     {this.state.categories.map(cat => {return(
-                        <CollectionSmall type="category" link={cat.id} colTitle={cat.name} src="https://www.logodesignlove.com/wp-content/uploads/2019/04/extinction-symbol-01.jpg" />
+                        <CollectionSmall type="category" link={cat.id} colTitle={cat.name} src={cat.image} />
                      )})}
                 </div>
                 </div>
